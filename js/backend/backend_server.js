cf0a22e703db738f7137d127c04f71feff730bec
George <georgelemach@gmail.com>
2024-09-07 17:33:41 +0300

    "Downloaded cors, for fixing tjhe issue with the frontend/backend communications (not resolved yet)".

--- js/backend/backend_server.js
+++ js/backend/backend_server.js
@@ -2,8 +2,12 @@ console.log("Starting the server...");
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+// enable cors for all routes
+app.use(cors());
 
 // Parse JSON bodies
 app.use(bodyParser.json());
